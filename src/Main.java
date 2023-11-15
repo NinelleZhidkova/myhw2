3f04952d3887f6a28888ece358c72ae97429c17e
Ninelle test <niserji@mail.ru>
2023-11-16 00:05:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 13676; // стоимость билета
+        int rubles = 20;  // рубли для одной бонусной мили
+
+
+        System.out.println("Рассчитали количество бонусных миль:");
+        System.out.println((price / rubles) + " миль ");
+    }
+}
